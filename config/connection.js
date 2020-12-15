5e4d1d1bd7503c2bf295284221b5e8bc35bad181
Baur Kereibayev <kbaur775@gmail.com>
2020-12-15 00:47:38 -0600

    making db configurable

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,11 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({host: "localhost", port: 3306, user: "root", database: "burgers_db"});
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({host: "localhost", port: 3306, user: "root", database: "burgers_db"});
+}
 connection.connect();
 
 module.exports = connection;
